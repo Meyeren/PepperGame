df0f2f8ae575d83bd714cb4a785347e0ca039fe7
Meyeren <lucasmeyergaming@gmail.com>
2025-04-08 13:49:07 +0200

    Player Jump + Staminabar

--- Assets/_Script/PlayerMovement.cs
+++ Assets/_Script/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System.Net.Mime;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -11,50 +12,102 @@ public class PlayerMovement : MonoBehaviour
     Transform groundCheck;
     LayerMask groundLayer;
 
-    [SerializeField] float Speed = 1.0f;
-    [SerializeField] float jumpPower = 16.0f;
+    [SerializeField] float Speed = 8.0f;
 
-    bool isGrounded;
+    [SerializeField] float Stamina = 100f;
+
+    [SerializeField] float jumpPower = 15.0f;
+    [SerializeField] float fallStop = 1.0f;
+    [SerializeField] float fallSpeed = 1.0f;
+    [SerializeField] float jumpCost = 50f;
+    [SerializeField] float staminaRegenAmount = 10f;
+
+    Slider staminaSlider;
+    CanvasGroup staminaCanvasGroup;
+
     private void Start()
     {
+        staminaSlider = GameObject.Find("StaminaBar").GetComponent<Slider>();
+        staminaCanvasGroup = staminaSlider.GetComponentInParent<CanvasGroup>();
+        staminaCanvasGroup.alpha = 0f;
+
         input = GetComponent<PlayerInput>();
+        rb = GetComponent<Rigidbody>();
+
         moveAction = input.actions.FindAction("Move");
         jumpAction = input.actions.FindAction("Jump");
 
-        rb = GetComponent<Rigidbody>();
         groundCheck = transform.Find("GroundCheck");
         groundLayer = LayerMask.GetMask("Ground");
+
+        Stamina = 100f;
 
 
     }
 
     private void Update()
     {
-        MovePlayer();
-        CheckGroundStatus();
         HandleJump();
-
+        StaminaRegen();
+        
+        staminaSlider.value = Stamina;
+        if (Stamina == 100)
+        {
+            FadeOutStaminaBar();
+        }
+        else
+        {
+            staminaCanvasGroup.alpha = 1f;
+        }
+        
     }
 
-    
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+
 
     private void MovePlayer()
     {
         Vector2 direction = moveAction.ReadValue<Vector2>();
-        rb.linearVelocity = new Vector3(direction.x * Speed, 0, direction.y * Speed);
+
+        rb.linearVelocity = new Vector3(direction.x * Speed, rb.linearVelocity.y, direction.y * Speed);
     }
 
-    void CheckGroundStatus()
+    bool IsGrounded()
     {
-        isGrounded =  Physics.OverlapSphere(groundCheck.position, 0.2f, groundLayer).Length > 0;
+        return Physics.OverlapSphere(groundCheck.position, 0.2f, groundLayer).Length > 0f;
     }
 
     void HandleJump()
     {
-        if (isGrounded && jumpAction.triggered)
+        if (IsGrounded() && jumpAction.triggered && Stamina >= jumpCost)
         {
-            rb.linearVelocity = new Vector3(rb.linearVelocity.x, jumpPower, rb.linearVelocity.z);
+            rb.linearVelocity = new Vector3(rb.linearVelocity.x,jumpPower, rb.linearVelocity.z);
+            Stamina -= jumpCost;
+        }
+        if (jumpAction.WasReleasedThisFrame() && rb.linearVelocity.y > 0f)
+        {
+            rb.linearVelocity = new Vector3(rb.linearVelocity.x, rb.linearVelocity.y * fallStop + (-fallSpeed), rb.linearVelocity.z);
+        }
+    }
 
+    void StaminaRegen()
+    {
+        if (IsGrounded() && Stamina > 0f)
+        {
+            Stamina += staminaRegenAmount * Time.deltaTime;
+            Stamina = Mathf.Clamp(Stamina, 0, 100);
+        }
+    }
+
+    void FadeOutStaminaBar()
+    {
+        if (staminaCanvasGroup.alpha > 0f)
+        {
+            staminaCanvasGroup.alpha -= Time.deltaTime * 0.5f;
         }
     }
 }
