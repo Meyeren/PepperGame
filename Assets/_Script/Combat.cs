87c0f1310184480c4a8f4dfea131f32134ab1024
Meyeren <lucasmeyergaming@gmail.com>
2025-04-23 10:35:27 +0200

    Healthbar

--- Assets/_Script/Combat.cs
+++ Assets/_Script/Combat.cs
@@ -1,8 +1,28 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class Combat : MonoBehaviour
 {
     public int playerHealth = 100;
+
+    Slider Health;
+
+
+    private void Start()
+    {
+        Health = GameObject.Find("Healthbar").GetComponent<Slider>();
+        Health.maxValue = playerHealth;
+    }
+
+    private void Update()
+    {
+        Health.value = playerHealth;
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            playerHealth -= 10;
+        }
+    }
+
 
 }
