e4308422897edd69ee918a424922e87ee5cc8996
Meyeren <lucasmeyergaming@gmail.com>
2025-04-28 13:26:46 +0200

    bally

--- Assets/_Script/EnemyHealth.cs
+++ Assets/_Script/EnemyHealth.cs
@@ -1,14 +1,24 @@
 using UnityEngine;
+using System.Collections;
 
 public class EnemyHealth : MonoBehaviour
 {
     public float enemyHealth = 100f;
 
+
+    public SkinnedMeshRenderer enemyRenderer;
+    MaterialPropertyBlock propBlock;
+
+
     private FlockingTest flockingTest;
 
     private void Start()
     {
+        enemyRenderer = GetComponentInChildren<SkinnedMeshRenderer>();
+        
         flockingTest = GetComponent<FlockingTest>();
+
+        propBlock = new MaterialPropertyBlock();
     }
 
     private void Update()
@@ -23,5 +33,25 @@ public class EnemyHealth : MonoBehaviour
     public void TakeDamage(float Damage)
     {
         enemyHealth -= Damage;
+        StartCoroutine(FlashRed());
+        
+    }
+
+    private IEnumerator FlashRed()
+    {
+
+        enemyRenderer.GetPropertyBlock(propBlock);
+
+
+        propBlock.SetColor("_Color", Color.red);
+
+        enemyRenderer.SetPropertyBlock(propBlock);
+
+        yield return new WaitForSeconds(0.2f);
+
+
+       
+   
+        enemyRenderer.SetPropertyBlock(propBlock);
     }
 }
